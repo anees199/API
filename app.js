3b4c894cbc3185b14782b98b99a4fea9e51b486c
anees199 <anees4865577@gmail.com>
2024-11-18 13:38:19 -0800

    work done css left

---
+++ app.js
@@ -0,0 +1,26 @@
+const div = document.querySelector(".container");
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.products);
+    res.products.map((item) => {
+      div.innerHTML += `<div class="card">
+            <img src=${item.thumbnail}
+                alt="thumbnail">
+            <h1>${item.title}</h1>
+            <p>${item.description.slice(0, 20)}...</p>
+            <p>Price: $${item.price}</p>
+            <button onclick="showMore(${item.id})">See more</button>
+        </div>`;
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const showMore = (id) => {
+  console.log(id);
+  localStorage.setItem("id", id);
+  window.location = "detail.html";
+};
